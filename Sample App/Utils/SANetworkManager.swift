4e7a8f731d2a3f9109a6c1548d72f9d9341ebef2
Raees <raeesmadathil@gmail.com>
2023-05-04 08:53:15 +0400

    -Base view controller added as base class for view controllers -Main view controller added to show most popular articles -Details view controller added to show article details -Network manager add to handle network related functions

---
+++ Sample App/Utils/SANetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  SANetworkManager.swift
+//  Sample App
+//
+//  Created by Raees  on 04/05/2023.
+//  Copyright © 2023 Sample. All rights reserved.
+//
+
+import UIKit
+
+/// This class is to handle all network related functions
+class SANetworkManager: NSObject {
+
+}
